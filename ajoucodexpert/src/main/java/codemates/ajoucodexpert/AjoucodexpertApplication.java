71c9032badab3bd5a6f8ebe2971cdfb4a238d088
MyeongQ <gmk0904@ajou.ac.kr>
2023-11-28 20:12:08 +0900

    [Feat] Create New Project

---
+++ ajoucodexpert/src/main/java/codemates/ajoucodexpert/AjoucodexpertApplication.java
@@ -0,0 +1,13 @@
+package codemates.ajoucodexpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjoucodexpertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AjoucodexpertApplication.class, args);
+    }
+
+}
---
+++ ajoucodexpert/src/test/java/codemates/ajoucodexpert/AjoucodexpertApplicationTests.java
@@ -0,0 +1,13 @@
+package codemates.ajoucodexpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjoucodexpertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
