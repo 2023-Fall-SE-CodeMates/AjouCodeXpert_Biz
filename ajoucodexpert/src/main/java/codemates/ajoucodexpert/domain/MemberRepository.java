56e3eb98278f31955cb1289259c866fba41325c5
MyeongQ <gmk0904@ajou.ac.kr>
2023-11-29 13:27:02 +0900

    [Feat] MemberRepository findById() 추가

--- ajoucodexpert/src/main/java/codemates/ajoucodexpert/domain/MemberRepository.java
+++ ajoucodexpert/src/main/java/codemates/ajoucodexpert/domain/MemberRepository.java
@@ -2,7 +2,9 @@ package codemates.ajoucodexpert.domain;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository {
-
+    Optional<Member> findById(String loginId);
 }
