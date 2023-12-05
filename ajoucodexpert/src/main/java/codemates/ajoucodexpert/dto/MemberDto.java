a24643d24cbc7df2b777fa879eb17204cca577d8
MyeongQ <gmk0904@ajou.ac.kr>
2023-12-05 15:28:39 +0900

    [Feat] MemberDto - login, signup 생성

--- ajoucodexpert/src/main/java/codemates/ajoucodexpert/dto/MemberDto.java
+++ ajoucodexpert/src/main/java/codemates/ajoucodexpert/dto/MemberDto.java
@@ -1,2 +1,30 @@
-package codemates.ajoucodexpert.dto;public class MemberDto {
+package codemates.ajoucodexpert.dto;
+
+import codemates.ajoucodexpert.domain.Member;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class MemberDto {
+    @Data
+    @Builder
+    @AllArgsConstructor @NoArgsConstructor
+    public static class Login{
+        private String email;
+        private String pw;
+    }
+
+    @Data
+    @Builder
+    @AllArgsConstructor @NoArgsConstructor
+    public static class Signup {
+        private Long id;
+        private String pw;
+        private String name;
+        private String studentId;
+        private String majorCode;
+        private Integer roleCode;
+
+    }
 }
