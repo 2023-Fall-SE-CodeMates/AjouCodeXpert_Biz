d1934d138d6922da0d0cdc5b9132b2743d3d2d7c
MyeongQ <gmk0904@ajou.ac.kr>
2023-12-05 22:24:42 +0900

    [Feat] MemberFactory (회원 객체 생성) service 구현

--- ajoucodexpert/src/main/java/codemates/ajoucodexpert/service/MemberFactory.java
+++ ajoucodexpert/src/main/java/codemates/ajoucodexpert/service/MemberFactory.java
@@ -1,2 +1,12 @@
-package codemates.ajoucodexpert.service;public interface MemberFactory {
+package codemates.ajoucodexpert.service;
+
+import codemates.ajoucodexpert.domain.Major;
+import codemates.ajoucodexpert.domain.Member;
+import codemates.ajoucodexpert.domain.StudentInfo;
+import codemates.ajoucodexpert.dto.MemberDto;
+
+public interface MemberFactory {
+    // MemberDto.Sigup, StudentInfo, Major -> Member를 생성하는 메서드
+    Member createMember(MemberDto.Signup signupDto, StudentInfo studentInfo, Major major);
+    Member saveMember(Member member);
 }
--- ajoucodexpert/src/main/java/codemates/ajoucodexpert/service/MemberFactoryImpl.java
+++ ajoucodexpert/src/main/java/codemates/ajoucodexpert/service/MemberFactoryImpl.java
@@ -1,2 +1,40 @@
-package codemates.ajoucodexpert.service;public class MemberFactoryImpl {
+package codemates.ajoucodexpert.service;
+
+import codemates.ajoucodexpert.domain.Major;
+import codemates.ajoucodexpert.domain.Member;
+import codemates.ajoucodexpert.domain.StudentInfo;
+import codemates.ajoucodexpert.dto.MemberDto;
+import codemates.ajoucodexpert.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class MemberFactoryImpl implements MemberFactory{
+    private final MemberRepository memberRepository;
+
+
+    @Override
+    public Member createMember(MemberDto.Signup signupDto, StudentInfo studentInfo, Major major) {
+        log.debug("새 Member 생성");
+        Member newMember =  Member.builder()
+                .loginId(signupDto.getId())
+                .pw(signupDto.getPw())
+                .name(signupDto.getName())
+                .studentInfo(studentInfo)
+                .major(major)
+                .build();
+
+        return saveMember(newMember);
+    }
+
+    @Override
+    public Member saveMember(Member unsaved) {
+        Member saved = memberRepository.save(unsaved);
+        log.info("새 Member 저장: {}", saved.getLoginId());
+        return saved;
+    }
 }
