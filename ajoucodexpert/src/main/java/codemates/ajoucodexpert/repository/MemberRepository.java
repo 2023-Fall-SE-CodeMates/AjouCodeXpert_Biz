88c0306cce04681ee8e4b4613d61da9d2d669150
MyeongQ <gmk0904@ajou.ac.kr>
2023-12-05 22:26:46 +0900

    [Feat] MemberRepository existsById, existsByStudentInfo 구현

--- ajoucodexpert/src/main/java/codemates/ajoucodexpert/repository/MemberRepository.java
+++ ajoucodexpert/src/main/java/codemates/ajoucodexpert/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package codemates.ajoucodexpert.repository;
 
 import codemates.ajoucodexpert.domain.Member;
+import codemates.ajoucodexpert.domain.StudentInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findById(String loginId);
+    boolean existsById(String loginId);
+    boolean existsByStudentInfo(StudentInfo studentInfo);
 }
