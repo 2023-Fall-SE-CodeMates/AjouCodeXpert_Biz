d3d31ccc0f718296380df27fae580f90cbefa3b4
MyeongQ <gmk0904@ajou.ac.kr>
2023-12-07 18:16:05 +0900

    [Feat] UpdateRoleRequestRepository 인터페이스 정의

--- ajoucodexpert/src/main/java/codemates/ajoucodexpert/repository/UpdateRoleRequestRepository.java
+++ ajoucodexpert/src/main/java/codemates/ajoucodexpert/repository/UpdateRoleRequestRepository.java
@@ -1,11 +1,11 @@
 package codemates.ajoucodexpert.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import codemates.ajoucodexpert.domain.UpdateRoleRequest;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UpdateRoleRequest extends JpaRepository<UpdateRoleRequest, Long> {
-    List<UpdateRoleRequest> findAllByRequestStatusEquals(int requestStatus);
+public interface UpdateRoleRequestRepository extends UserRequestRepository<UpdateRoleRequest> {
+    List<UpdateRoleRequest> findAllByRequestStatus(int requestStatus);
 }
--- ajoucodexpert/src/main/java/codemates/ajoucodexpert/repository/UserRequestRepository.java
+++ ajoucodexpert/src/main/java/codemates/ajoucodexpert/repository/UserRequestRepository.java
@@ -1,2 +1,9 @@
-package codemates.ajoucodexpert.repository;public class UserRequestRepository {
+package codemates.ajoucodexpert.repository;
+
+import codemates.ajoucodexpert.domain.UserRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface UserRequestRepository<T extends UserRequest> extends JpaRepository<T, Long> {
 }
